0a7103b9855f5e3c30e60d3697e7fd2c24844b6d
David Castillo Alvarez <deivizcastle@gmail.com>
2020-02-01 13:19:50 +0100

    "Ejercicio 9 de la Unidad 4 RESUELTO"

--- Ejercicios 2ª Evaluación/src/unidad4/Ejercicio09.java
+++ Ejercicios 2ª Evaluación/src/unidad4/Ejercicio09.java
@@ -6,48 +6,44 @@ public class Ejercicio09 {
 
 		int tamaño = (int) (Math.random() * (501 - 10) + 10);
 		int[] vector = new int[tamaño];
-
-		llenarVector(vector);
 		
+		llenarVector(vector);
+		System.out.println();
+		secuencias(vector);
 
 	}
 
 	public static void llenarVector(int[] vector) {
-		int contador=0;
+		int contador = 0;
 		for (int i = 0; i < vector.length; i++) {
 			int numero = (int) (Math.random() * (101 - (-100)) + (-100));
 			vector[i] = numero;
 			if (vector.length <= 50) {
 				System.out.print(" " + vector[i]);
 			}
-			
-			
+
 		}
-		if (vector[1]==vector[0]) {
-			contador++;
-			for (int j=2; j<vector.length; j++) {
-				if (vector[j]==vector[j-1]) {
-					contador++;
-				}
-				
-			}
-		}
-		for (int j=2; j<vector.length; j++) {
+
+	}
+
+	public static void secuencias(int[] vector) {
 			
-			if (vector[j]==vector[j-1]) {
-				contador++;
-			}
-			
-		}
-	
-		if (vector.length>50) {
-			System.out.println("El vector es mayor de 50");
-		}
-		System.out.println();
-		System.out.println("Secuencias: "+contador);
+		boolean haySecuencia=false;
+		int contadorSecuencias=0;
 		
+		for (int i=1; i<vector.length; i++) {
+			
+			if (vector[i]==vector[i-1] && !haySecuencia) {
+				contadorSecuencias++;
+				haySecuencia=true;
+			}
+			if (vector[i]==vector[i-1]) {
+				haySecuencia=false;
+			}
+		}
+		
+		System.out.println("Número de secuencias: "+contadorSecuencias);
 		
 	}
 
-	
 }
