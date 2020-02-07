af31ee7f8af15589542f5fa439f2389ae4a19d0e
David Castillo Alvarez <deivizcastle@gmail.com>
2020-02-07 17:13:21 +0100

    "Ejercicio 2 Unidad 4(colecciones) resuelto"(completo)

--- Ejercicios 2ª Evaluación/src/unidad4_colecciones/Ejercicio02.java
+++ Ejercicios 2ª Evaluación/src/unidad4_colecciones/Ejercicio02.java
@@ -5,29 +5,44 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		
 		List <Integer>lista= new ArrayList<>();
+		Set <Integer>coleccion1 = new HashSet<>();
+		Set <Integer>coleccion2 = new TreeSet<>();
 		
 		for (int i=0; i<100; i++) {
 			int numero=(int)(Math.random()*(101-1)+1);
 			lista.add(numero);
+			
+			
 		}
+		
+		coleccion1.addAll(lista);
+		coleccion2.addAll(lista);
 						
 		Iterator<Integer> i=lista.iterator();
-		
 		while (i.hasNext()) {
 						
 			System.out.print(" "+i.next());
 		}
 		
-		/*Set <Integer> c=new HashSet<Integer>();
-		c.add(lista);*/
+		System.out.println();
 		
 		
+		
+		for (Integer j: coleccion1) {
+			System.out.print(j+" ");
+			
+		}
+		
+		System.out.println();
+		
+		coleccion2.forEach(numero -> System.out.print(numero+" "));
 		}
 
 	}
