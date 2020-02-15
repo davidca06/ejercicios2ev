bbf94cb951182f89bfdee7381cc6a0f185ab51ff
David Castillo Alvarez <deivizcastle@gmail.com>
2020-02-15 13:51:05 +0100

    "Ejercicio 15 de la Unidad 4 resuelto"

--- Ejercicios 2ª Evaluación/src/unidad4/Ejercicio15.java
+++ Ejercicios 2ª Evaluación/src/unidad4/Ejercicio15.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ejercicio15 {
 
 	static Scanner in = new Scanner(System.in);
+	
 
 	public static void main(String[] args) {
 
@@ -24,7 +25,8 @@ public class Ejercicio15 {
 
 		for (int i = 0; i < alumnos.length; i++) {
 			System.out.println("Nombre del alumno: ");
-			String nombre = in.next();
+			in.nextLine();
+			String nombre = in.nextLine();
 			alumnos[i] = nombre;
 			System.out.println("Primera Evaluación: ");
 			float nota1 = in.nextFloat();
@@ -36,14 +38,7 @@ public class Ejercicio15 {
 			float nota3 = in.nextFloat();
 			notas[i][2] = nota3;
 		}
-		System.out.println("------------------");
-		for (int j = 0; j < notas.length; j++) {
-			System.out.print(alumnos[j] + " ");
-			for (int k = 0; k < notas[j].length; k++) {
-				System.out.print(notas[j][k] + " ");
-			}
-			System.out.println();
-		}
+		
 
 	}
 
@@ -51,6 +46,7 @@ public class Ejercicio15 {
 
 		int op = 0;
 		do {
+			System.out.println();
 			System.out.println("Elige una opción (1-7): ");
 			System.out.println("1. Nota media de todos los alumnos");
 			System.out.println("2. Nota media de un alumno");
@@ -72,28 +68,30 @@ public class Ejercicio15 {
 
 		switch (op) {
 		case 1:
-			notaMediaCadaAlumno(alumnos, notas);
+			notaMediaTodosAlumno(alumnos, notas);
 			break;
 		case 2:
 			notaMediaAlumno(alumnos, notas);
 			break;
 		
 		case 3:
-			/*notaEvaluacion(alumnos, notas);
-			notaFinalEvaluacion(alumnos, notas);*/
+			notaEvaluacion(alumnos, notas);
 			break;
 					
 		case 4:
+			notaEvaluacionAlumno(alumnos,notas);
 			break;
 		case 5:
 			notaMediaCurso(alumnos, notas);
 			break;
 		case 6:
+			notaMasAlta(alumnos,notas);
+			notaMasBaja(alumnos,notas);
 			break;	
 		}
 	}
 
-	public static void notaMediaCadaAlumno(String[] alumnos, float[][] notas) {
+	public static void notaMediaTodosAlumno(String[] alumnos, float[][] notas) {
 		float sumaNotas;
 		float mediaAlumno;
 		for (int i=0; i<notas.length; i++) {
@@ -101,11 +99,10 @@ public class Ejercicio15 {
 			sumaNotas=0;
 			for (int j=0; j<notas[i].length; j++) {
 				sumaNotas=sumaNotas+notas[i][j];
-			System.out.print(notas[i][j]+" ");
-						
+									
 			}
-			System.out.println();
-			System.out.println("Nota Media: "+sumaNotas/3);
+			
+			System.out.printf("Nota Media: %.1f\n\n",sumaNotas/3);
 		}
 		
 	}
@@ -115,7 +112,8 @@ public class Ejercicio15 {
 		float mediaAlumno;
 		boolean fin = false;
 		System.out.println("Nombre del alumno: ");
-		String nombreAlumno = in.next();
+		in.nextLine();
+		String nombreAlumno = in.nextLine();
 		do {
 		for (int i = 0; i < alumnos.length; i++) {
 			if (alumnos[i].equalsIgnoreCase(nombreAlumno)) {
@@ -127,8 +125,51 @@ public class Ejercicio15 {
 		}
 		} while (!fin);
 		mediaAlumno = sumaNotas / 3;
-		System.out.println("La nota media del alumno es: " + mediaAlumno);
-		System.out.println();
+		System.out.printf("La nota media del alumno es: %.1f\n\n",mediaAlumno);
+		
+	}
+	
+	public static void notaEvaluacion(String[] alumnos, float[][] notas) {
+		
+		float [] vectorNotaFinal=new float[alumnos.length];
+		
+		System.out.println("----- NOTAS ----");
+		for (int j = 0; j < notas.length; j++) {
+			System.out.print(alumnos[j] + ": ");
+			for (int k = 0; k < notas[j].length; k++) {
+				System.out.print(" ... "+notas[j][k]);
+				vectorNotaFinal[j]+=notas[j][k];
+			}
+			System.out.printf(" ...Nota Final Evaluación: %.1f",vectorNotaFinal[j]/3);
+			System.out.println();
+		}
+		
+	}
+	
+	public static void notaEvaluacionAlumno(String[] alumnos, float[][] notas) {
+		
+		float [] vectorNotaFinal=new float[alumnos.length];
+		boolean fin = false;
+		System.out.println("Nombre del alumno: ");
+		in.nextLine();
+		String nombreAlumno = in.nextLine();
+		do {
+		for (int i = 0; i < alumnos.length; i++) {
+			if (alumnos[i].equalsIgnoreCase(nombreAlumno)) {
+				System.out.print(alumnos[i] + ": ");
+					for (int j = 0; j < notas[i].length; j++) {
+						System.out.print(" ... "+notas[i][j]);
+						vectorNotaFinal[i]+=notas[i][j];
+					}
+					System.out.printf(" ...Nota Final Evaluación: %.1f",vectorNotaFinal[i]/3);
+					System.out.println();
+					
+				}
+			fin=true;
+			}
+		
+		} while (!fin);
+		
 	}
 	
 	public static void notaMediaCurso(String[] alumnos, float[][] notas) {
@@ -141,7 +182,61 @@ public class Ejercicio15 {
 
 		}
 		mediaTotal = sumaNotas / (alumnos.length * 3);
-		System.out.println("La nota media de todos los alumnos es: " + mediaTotal);
+		System.out.printf("La nota media del curso es: %.1f",mediaTotal);
 		System.out.println();
+	}
+	
+	public static void notaMasAlta (String[] alumnos, float[][] notas) {
+		
+		float notaMayor=0;
+		String evaluacion="";
+		String alumno="";
+		
+		for (int i=0; i<notas.length; i++) {
+			for (int j=0; j<notas[i].length; j++) {
+				if (notas[i][j]>notaMayor) {
+					notaMayor=notas[i][j];
+					if (j==0) {
+						evaluacion="1a Evaluacion";
+					}
+					else if (j==1) {
+						evaluacion="2a Evaluacion";
+					}
+					else {
+						evaluacion="3a Evaluacion";
+					}
+					alumno=alumnos[i];
+					
+				}
+			}
+		}
+		System.out.println("La nota más alta es: "+notaMayor+" de la "+evaluacion+" y pertenece al alumno "+alumno);
+	}
+	
+public static void notaMasBaja (String[] alumnos, float[][] notas) {
+		
+		float notaMenor=10;
+		String evaluacion="";
+		String alumno="";
+		
+		for (int i=0; i<notas.length; i++) {
+			for (int j=0; j<notas[i].length; j++) {
+				if (notas[i][j]<notaMenor) {
+					notaMenor=notas[i][j];
+					if (j==0) {
+						evaluacion="1a Evaluacion";
+					}
+					else if (j==1) {
+						evaluacion="2a Evaluacion";
+					}
+					else {
+						evaluacion="3a Evaluacion";
+					}
+					alumno=alumnos[i];
+					
+				}
+			}
+		}
+		System.out.println("La nota más baja es: "+notaMenor+" de la "+evaluacion+" y pertenece al alumno "+alumno);
 	}
 }
