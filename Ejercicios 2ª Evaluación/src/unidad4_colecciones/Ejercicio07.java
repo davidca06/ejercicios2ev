688f95036a4a84f9959f77b549400cbfed0c1ea1
David Castillo Alvarez <deivizcastle@gmail.com>
2020-03-01 16:36:34 +0100

    "Ejercicio 7 de la Unidad 4 (colecciones) resuelto"

--- Ejercicios 2ª Evaluación/src/unidad4_colecciones/Ejercicio07.java
+++ Ejercicios 2ª Evaluación/src/unidad4_colecciones/Ejercicio07.java
@@ -14,8 +14,7 @@ public class Ejercicio07 {
 		Scanner in = new Scanner(System.in);
 
 		TreeMap<String, HashSet> juegoNaipes = new TreeMap<String, HashSet>();
-		String nombre=" ";
-		
+		String nombre = " ";
 
 		boolean fin = false;
 
@@ -28,10 +27,9 @@ public class Ejercicio07 {
 				fin = true;
 			} else {
 				nombre = linea.substring(0, linea.indexOf(" "));
-				//System.out.print(nombre + ": ");
 				String tirada = linea.substring(linea.indexOf(":") + 2);
 				naipe = tirada.split(" ");
-				
+
 				if (juegoNaipes.containsKey(nombre)) {
 					HashSet<String> naipes = juegoNaipes.get(nombre);
 					for (int i = 0; i < naipe.length; i++) {
@@ -44,126 +42,64 @@ public class Ejercicio07 {
 						juegoNaipes.put(nombre, naipes);
 					}
 				}
-				
-				}
-					
+
+			}
+
 		} while (!fin);
-		
+
 		System.out.println("Salida: ");
 		Set nombres = juegoNaipes.keySet();
 		Iterator it = nombres.iterator();
-		while (it.hasNext()) { 
-			nombre = (String)it.next();
-			//System.out.print(nombre+": ");
+		while (it.hasNext()) {
+			nombre = (String) it.next();
 			HashSet naipes = juegoNaipes.get(nombre);
-			Iterator it2=naipes.iterator();
-			System.out.print(nombre+": ");
+			Iterator it2 = naipes.iterator();
+			System.out.print(nombre + ": ");
+			int puntos = 0;
 			while (it2.hasNext()) {
-				/*String valorTemp=(String)it2.next();
-				Integer valor=Integer.parseInt(valorTemp);
-				valor=calcularValor(naipe);
-				System.out.print(valor);*/
-				System.out.print(it2.next()+", ");					
-			}
-			
-			System.out.println();
-					
-		}
-		/*System.out.println("Mano por jugador: ");
-		Set nombres = juegoNaipes.keySet();
-		Iterator it = nombres.iterator();
-		while (it.hasNext()) { 
-			nombre = (String)it.next();
-			HashSet naipes = juegoNaipes.get(nombre);
-			Iterator it2=naipes.iterator();
-			System.out.print(nombre+": ");
-			while (it2.hasNext()) {
-				
-				System.out.print(it2.next()+", ");
-				
-			}
-			System.out.println();*/
-		
-		//calcularValor(naipe);
-}
-	public static int calcularValor (String []naipe) {
-		int valorRango=0;
-		int valorPalo;
-		int valorNaipe=0;
-		
-		for (int i=0; i<naipe.length; i++) {
-			String rango=naipe[i].substring(0,1);
-			//System.out.println(rango);
-			switch (rango) {
-			case "2":
-				valorRango=2;
-				break;
-			case "3":
-				valorRango=3;
-				break;
-			case "4":
-				valorRango=4;
-				break;
-			case "5":
-				valorRango=5;
-				break;
-			case "6":
-				valorRango=6;
-				break;
-			case "7":
-				valorRango=7;
-				break;
-			case "8":
-				valorRango=8;
-				break;
-			case "9":
-				valorRango=9;
-				break;
-			case "10":
-				valorRango=10;
-				break;
-			case "J":
-				valorRango=11;
-				break;
-			case "Q":
-				valorRango=12;
-				break;
-			case "K":
-				valorRango=13;
-				break;
-			case "A":
-				valorRango=14;
-				break;
+				String jugada = (String) it2.next();
+				String rango = jugada.substring(0, jugada.length() - 1);
+				int valorRango = 0;
+				switch (rango) {
+				case "J":
+					valorRango = 11;
+					break;
+				case "Q":
+					valorRango = 12;
+					break;
+				case "K":
+					valorRango = 13;
+					break;
+				case "A":
+					valorRango = 14;
+					break;
 				default:
-					System.out.println("Rango no válido");
+					valorRango = Integer.parseInt(rango);
+				}
+				String palo = jugada.substring(jugada.length() - 1);
+				int valorPalo = 0;
+				switch (palo) {
+				case "S":
+					valorPalo = 4;
+					break;
+				case "H":
+					valorPalo = 3;
+					break;
+				case "D":
+					valorPalo = 2;
+					break;
+				case "C":
+					valorPalo = 1;
+					break;
+
+				}
+				puntos = puntos + valorRango * valorPalo;
+
 			}
-			String palo=naipe[i].substring(1);
-			//System.out.println(palo);
-			valorPalo=0;
-			switch (palo) {
-			case "S":
-				valorPalo=4;
-			break;
-			case "H":
-				valorPalo=3;
-			break;
-			case "D":
-				valorPalo=2;
-			break;
-			case "C":
-				valorPalo=1;
-			break;
-			default:
-				System.out.println("Palo no válido");
-			
-			}
-			
-			valorNaipe+=valorRango*valorPalo;
-			System.out.print(valorNaipe+" ");
-			
+			System.out.println(puntos);
+
 		}
-		
-		return valorNaipe;
-		
+
 	}
+
 }
